7c03b3d9779238a67fd744b64a63b41320443b53
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-15 14:38:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('hello mate academy!');
+const bigImage = document.querySelector('#largeImg');
+
+function onClick(even) {
+  even.preventDefault();
+
+  let src = even.toElement.src.split('-');
+
+  src.pop();
+  src = src.join('-').concat('.png');
+
+  bigImage.setAttribute('src', src);
+}
+
+const img1 = document.querySelectorAll(`.gallery__img`);
+
+for (let i = 0; i < img1.length; i++) {
+  img1[i].addEventListener('click', onClick);
+}
